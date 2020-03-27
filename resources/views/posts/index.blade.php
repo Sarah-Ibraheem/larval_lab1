5b28ed9e248cf0e2ef95026bff8a512359a32147
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-03-27 03:41:53 +0200

    show user name

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -25,7 +25,7 @@
           <tr>
                                 <td>{{$post->id}}</td>
 								<td>{{$post->title}}</td>
-								<td>{{$post->user_id}}</td>
+								<td>{{$post->user->name}}</td>
 								<td>{{$post->created_at}}</td>
 								<td>{{$post->updated_at}}</td>
 								<td>{{$post->description}}</td>
