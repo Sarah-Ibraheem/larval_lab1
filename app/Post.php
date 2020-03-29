70aede11e383748e0dcdfd08378b3da9673fc39f
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-03-29 21:56:52 +0200

    add sluggable function

--- app/Post.php
+++ app/Post.php
@@ -3,18 +3,38 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
 
 class Post extends Model
 {
+    use Sluggable;
     protected $fillable = [
         'title',
         'description',
         'user_id',
+        'slug',
     ];
 
     
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
