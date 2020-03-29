13fc5cca6e1ec9f36675d7e536e9a284ae5a8c4d
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-03-29 13:42:22 +0200

    remove validation from create view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,35 +7,29 @@
         <div class="row">
           <div class="col-md-6 mb-3">
             <label for="firstName">Title</label>
-            <input type="text" class="form-control" id="firstName" placeholder="" value="" name="title" required>
-            <div class="invalid-feedback">
-              Valid Title is required.
-            </div>
+            <input type="text" class="form-control" id="firstName" placeholder="" value="" name="title" >
+            
           </div>
         </div>
 
         <div class="mb-3">
           <label for="username">Description</label>
           <div class="input-group">
-            <textarea class="form-control" id="username" placeholder="" required name="description"></textarea>
-            <div class="invalid-feedback" style="width: 100%;">
-              Your desc is required.
-            </div>
+            <textarea class="form-control" id="username" placeholder="" name="description"></textarea>
+            
           </div>
         </div>
 
        
           <div class="col-md-4 mb-3">
             <label for="state">Created_BY</label>
-            <select name="user_id" class="custom-select d-block w-100" id="state" required>
+            <select name="user_id" class="custom-select d-block w-100" id="state">
               <option value="">Choose User</option>
               @foreach ($users as $user)
               <option value="{{$user->id}}" >{{$user->name}}</option>
               @endforeach
             </select>
-            <div class="invalid-feedback">
-              Please choose user.
-            </div>
+           
           </div>
          
         <hr class="mb-4">
